9335689e5f4397807e7aa0be655b868de77d4324
Terry <terryfawden@gmail.com>
2026-04-22 09:34:19 +0100

    Added white detailing to the flashing dot

--- src/components/AnimatedCircle.tsx
+++ src/components/AnimatedCircle.tsx
@@ -16,7 +16,7 @@ export const AnimatedCircle: React.FC = () => {
     <Box
       style={{
         width: '100%',
-        // height: '100%',
+        height: '100%',
         minHeight: '400px',
         backgroundColor: 'black',
         transition: 'background-color 0.1s',
@@ -32,11 +32,27 @@ export const AnimatedCircle: React.FC = () => {
           width: '90px',
           height: '90px',
           borderRadius: '50%',
+          border: isVisible ? '2px solid white' : '2px solid black',
+          boxSizing: 'border-box',
           backgroundColor: isVisible ? 'red' : 'black',
           position: 'absolute',
           zIndex: 1,
         }}
-      />
+      >
+        <Box
+          style={{
+            width: '10px',
+            height: '10px',
+            borderRadius: '50%',
+            backgroundColor: isVisible ? 'white' : 'black',
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 2,
+          }}
+        />
+      </Box>
     </Box>
   );
 };
